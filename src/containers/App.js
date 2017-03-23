7fc954d95ef480997f9634e07f817b1eff832f59
TPoryadin <poryadinta@gmail.com>
2017-03-23 10:48:48 +0300

    add sidebar container and drawer

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,9 @@
-import React, {Component, PropTypes} from 'react';
+import React, {Component} from 'react';
 
 import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+
+import Sidebar from './sidebar';
 
 const titleStyle = {
     fontSize: '1.5rem',
@@ -9,15 +12,39 @@ const titleStyle = {
     fontWeight: 500
 };
 
+const sidebarOverlayStyle = {
+  backgroundColor: 'rgba(0, 0, 0, 0)'
+};
+
 class App extends Component {
+    state = {
+        isSidebarOpen: false
+    };
+    
+    toggleSidebar = () => {
+        this.setState(({isSidebarOpen}) => ({
+            isSidebarOpen: !isSidebarOpen
+        }));
+    };
     
     render () {
+        const {isSidebarOpen} = this.state;
+        
         return (
             <div className="app-container">
                 <AppBar title="ГЕОМОНИТОРИНГ ЗАЛОГОВ"
                         className="sber-appbar"
                         titleStyle={ titleStyle }
+                        onLeftIconButtonTouchTap={this.toggleSidebar}
                 />
+                <Drawer open={isSidebarOpen}
+                        docked={false}
+                        onRequestChange={this.toggleSidebar}
+                        overlayStyle={sidebarOverlayStyle}
+                        width={360}
+                >
+                    <Sidebar/>
+                </Drawer>
                 {this.props.children}
             </div>
         )
---
+++ src/containers/sidebar/index.js
@@ -0,0 +1,15 @@
+import React, {Component, PropTypes} from 'react';
+
+class Sidebar extends Component {
+    propTypes = {
+    
+    };
+    
+    render () {
+        return (
+            <div>Sidebar content</div>
+        )
+    }
+}
+
+export default Sidebar;
