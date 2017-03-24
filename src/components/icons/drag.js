fa2bb79526002cbcd321cddc39be669e46d7521b
TPoryadin <poryadinta@gmail.com>
2017-03-24 15:00:14 +0300

    SBER-41 add icon components

---
+++ src/components/icons/drag.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import SvgIcon from 'material-ui/SvgIcon';
+
+const DragIcon = (props) => (
+    <SvgIcon {...props} viewBox="0 0 5 11">
+        <circle cx="1" cy="10" r="1"/>
+        <circle cx="1" cy="7" r="1"/>
+        <circle cx="1" cy="4" r="1"/>
+        <circle cx="1" cy="1" r="1"/>
+        <circle cx="4" cy="10" r="1"/>
+        <circle cx="4" cy="7" r="1"/>
+        <circle cx="4" cy="4" r="1"/>
+        <circle cx="4" cy="1" r="1"/>
+    </SvgIcon>
+);
+
+export default DragIcon;
