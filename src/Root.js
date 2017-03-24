8590762b54f7b35f2b478bedd7e7b122a9cdea2d
TPoryadin <poryadinta@gmail.com>
2017-03-24 12:08:03 +0300

    replace theme to separate module

--- src/Root.js
+++ src/Root.js
@@ -2,21 +2,13 @@ import React, {Component, PropTypes} from 'react';
 import {Provider} from 'react-redux';
 import {BrowserRouter as Router} from 'react-router-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import {theme} from './assets/theme';
 
 import Routes from './routes';
 import App from './containers/App';
 
 import 'normalize.css/normalize.css';
 import './assets/fonts/fonts.css';
-
-const theme = getMuiTheme({
-    fontFamily: 'FedraSans',
-    appBar: {
-        height: 80,
-        color: '#64c76c'
-    }
-});
 
 class Root extends Component {
     static propTypes = {
---
+++ src/assets/theme/index.js
@@ -0,0 +1,12 @@
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+export const theme = getMuiTheme({
+    fontFamily: 'FedraSans',
+    appBar: {
+        height: 80,
+        color: '#64c76c'
+    },
+    svgIcon: {
+        color: '#d9dfe3'
+    }
+});
