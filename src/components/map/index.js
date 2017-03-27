b7a5047542c09e32145ccb10b2c70d8b3288184c
TPoryadin <poryadinta@gmail.com>
2017-03-27 14:53:45 +0300

    add sGis map example

--- src/components/map/index.js
+++ src/components/map/index.js
@@ -1,26 +1,31 @@
 import React, {Component, PropTypes} from 'react';
+import sGis from '../../assets/sgis';
 
 import './map.css';
 
 class Map extends Component {
     static propTypes = {
         center: PropTypes.array.isRequired,
-        onCenterChange: PropTypes.func.isRequired,
-        onResetMap: PropTypes.func.isRequired
+        onCenterChange: PropTypes.func.isRequired
     };
     
-    onButtonClick = () => {
-        const {center, onCenterChange} = this.props;
-        onCenterChange([center[0]-1, center[1]+1])
-    };
+    componentDidMount () {
+        const point = new sGis.Point([59.94, 30.43]).projectTo(sGis.CRS.webMercator);
+        const gis = sGis.init({
+            wrapper: this.container,
+            position: point.position,
+            resolution: 76.437,
+            layers: [ new sGis.TileLayer('http://b.tile.openstreetmap.org/{z}/{x}/{y}.png') ]
+        });
+        gis.map.on('click', function(sGisEvent) {
+            gis.map.animateTo(sGisEvent.point, gis.map.resolution / 2);
+        });
+    }
     
     render () {
-        const {center, onResetMap} = this.props;
         return (
             <div className="map-container" ref={container => this.container = container}>
-                <button onTouchTap={this.onButtonClick}>setCenter</button>
-                <button onTouchTap={onResetMap}>resetMap</button>
-                <span>{`[${center[0]}][${center[1]}]`}</span>
+    
             </div>
         );
     }
