c0f3fa9f110237d2cf1f5b550d8988a45c10206b
TPoryadin <poryadinta@gmail.com>
2017-03-27 13:14:24 +0300

    add colors.scss and lighgray color

--- src/assets/theme/index.js
+++ src/assets/theme/index.js
@@ -4,6 +4,7 @@ export const darkGrey = '#18191a';
 export const strongSilver = '#81888f';
 export const silver = '#d9dfe3';
 export const softGreen = '#64c76c';
+export const lightGray = '#f3f7ef';
 
 export const theme = getMuiTheme({
     fontFamily: 'FedraSans',
