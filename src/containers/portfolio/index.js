b54e9f6c8bcda35ef8c8b541c123a67863f3b260
ma <ma@everpoint.ru>
2017-03-28 19:27:08 +0300

    Merge branches

--- src/containers/portfolio/index.js
+++ src/containers/portfolio/index.js
@@ -1,4 +1,5 @@
 import React, {Component, PropTypes} from 'react';
+import { connect } from 'react-redux';
 
 import HeaderTitleBlock from '../../components/header-title-block';
 import PortfolioTable from '../../components/portfolio-table';
