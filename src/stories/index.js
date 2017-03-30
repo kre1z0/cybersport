757aaf95fb6e5c772f2a4ee1d5a1bb5e56bc816c
TPoryadin <poryadinta@gmail.com>
2017-03-30 11:03:06 +0300

    add rounded button component

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,2 +1,7 @@
+import 'reset.css/reset.css';
+import '../assets/fonts/fonts.css';
+import '../assets/base/main.css';
+
 import Map from './map';
+import Buttons from './buttons';
 import Icons from './icons';
