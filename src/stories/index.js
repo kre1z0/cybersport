47c83dc6f7be783f4e532fd4f0fa39108a5a5f0f
TPoryadin <poryadinta@gmail.com>
2017-03-30 13:59:12 +0300

    some stories fix

--- src/stories/index.js
+++ src/stories/index.js
@@ -2,6 +2,10 @@ import 'reset.css/reset.css';
 import '../assets/fonts/fonts.css';
 import '../assets/base/main.css';
 
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
 import Map from './map';
 import Buttons from './buttons';
+import ModalWindow from './modal-window';
 import Icons from './icons';
