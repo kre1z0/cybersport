63fd14881fa46fc3136d919b1b77f1367cd30f33
TPoryadin <poryadinta@gmail.com>
2017-03-31 17:08:52 +0300

    add colors, change secondary theme color

--- src/assets/theme/index.js
+++ src/assets/theme/index.js
@@ -6,6 +6,9 @@ export const silver = '#d9dfe3';
 export const softGreen = '#64c76c';
 export const lightGray = '#f3f7ef';
 export const paleGrey = '#dce1e6';
+export const macaroniAndCheese = '#f7c134';
+export const coolGrayTwo = '#9da3aa';
+export const dustyOrange = '#eb7f2e';
 
 export const theme = getMuiTheme({
     fontFamily: 'FedraSans',
@@ -17,6 +20,7 @@ export const theme = getMuiTheme({
         color: silver
     },
     palette: {
-        primary1Color: softGreen
+        primary1Color: softGreen,
+        accent1Color: dustyOrange
     }
 });
