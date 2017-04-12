429b2bfff0fecf309acf6b03b379d38e82656251
TPoryadin <poryadinta@gmail.com>
2017-04-12 14:06:00 +0300

    SBER-49 change cache options

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -244,7 +244,9 @@ module.exports = {
     new ManifestPlugin({
       fileName: 'asset-manifest.json'
     }),
-    new OfflinePlugin()
+    new OfflinePlugin({
+        responseStrategy: 'cache-first'
+    })
   ],
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
