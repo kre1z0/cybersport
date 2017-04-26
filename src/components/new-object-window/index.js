b481261062a2c525faf5d355fa2c149d44bf77c6
kreizo <thekreizo@gmail.com>
2017-04-26 16:13:25 +0300

    new object

--- src/components/new-object-window/index.js
+++ src/components/new-object-window/index.js
@@ -1,1 +1,41 @@
+import React, { Component } from 'react';
+import classNames from 'classnames';
+import './new-object-window.scss'
+import TextInput from '../text-input'
+import SelectField from '../select-field'
+import ImageLoader from '../img-loader'
 
+class newObjectWindow extends Component {
+  constructor(props){
+    super(props)
+  }
+  render() {
+    const { data } = this.props;
+    return (
+      <form className="new-object-window">
+        {data.map(({name, alias, type}) => {
+          let input;
+          if (type === 'text' || type === 'address') input = (<TextInput />);
+          else if (type === 'select') input = (<SelectField />);
+          else if (type === 'text-area') input = (<textarea />);
+          else if (type === 'text-string') input = (<div className="new-object-id">000003</div>);
+          else if (type === 'img') input = (<ImageLoader />);
+
+          const fieldGroup = classNames('field-group',
+            { top: type === 'text-area' || type === 'img' });
+
+          return (
+            <div className={fieldGroup} key={name}>
+              <label>{alias}</label>
+              <div className="input-wrapper">
+                {input}
+              </div>
+            </div>
+          )
+        })}
+      </form>
+    )
+  }
+}
+
+export default newObjectWindow;
