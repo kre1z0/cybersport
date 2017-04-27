279639288f853a51544470141b71c71f61bbb372
TPoryadin <poryadinta@gmail.com>
2017-04-27 14:38:23 +0300

    SBER-49 fix rounded button in stories

--- src/stories/columns-settings.js
+++ src/stories/columns-settings.js
@@ -5,7 +5,7 @@ import RaisedButton from 'material-ui/RaisedButton';
 
 import {theme} from '../assets/theme';
 import ModalWindow from '../components/modal-window';
-import RoundedButton from '../components/rounded-button';
+import RoundedButton from '../components/button/rounded-button';
 
 import {attributes} from './new-object-window'
 import ColumnsSettings from '../components/columns-settings'
