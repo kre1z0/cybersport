e31b40f8e4e39d593ce9ad4f892e28aa73bfc135
re <re@everpoint.ru>
2017-05-17 12:38:49 +0300

    noMUI

---
+++ src/components/noMUI/text-area/index.js
@@ -0,0 +1,14 @@
+/**
+ * Created by re on 17.05.2017.
+ */
+import React, {Component} from 'react';
+
+class TextArea extends Component {
+    render(){
+        return <div className="sberTextArea">
+            <textarea />
+        </div>
+    }
+}
+
+export default TextArea;
