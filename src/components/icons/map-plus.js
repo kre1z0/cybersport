e45e4dff64b37eadbbbcec7a0c954d5e16a4402b
kreiz0 <thekreizo@gmail.com>
2017-05-19 11:29:48 +0300

    map-controls-buttons

---
+++ src/components/icons/map-plus.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import SvgIcon from 'material-ui/SvgIcon'
+import { darkGrey } from '../../assets/theme'
+
+const MapPlus = ({ ...props }) => (
+  <SvgIcon {...props} viewBox="0 0 16 16" >
+    <path fill={darkGrey} d="M15,7H9V1A1,1,0,0,0,7,1V7H1A1,1,0,0,0,1,9H7v6a1,1,0,0,0,2,0V9h6a1,1,0,0,0,0-2Z"/>
+  </SvgIcon>
+);
+
+export default MapPlus;
---
+++ src/components/map-controls/map-control-button.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import IconButton from 'material-ui/IconButton'
+
+const buttonStyle = {
+  width: 44,
+  height: 44,
+  boxShadow: '0px 3px 10px 0 rgba(12, 13, 13, 0.1)',
+  backgroundColor: '#fff',
+  borderRadius: '50%'
+};
+
+const iconStyle = {
+  width: 16,
+  height: 16
+};
+
+const MapControlButton = ({ ico, isActive, ...props }) => (
+  <IconButton {...props} style={buttonStyle}
+    iconStyle={iconStyle}
+  >
+    {ico}
+  </IconButton>
+);
+
+export default MapControlButton;
