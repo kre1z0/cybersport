4dcc55067850464994a87e28a0841758883df32c
TPoryadin <poryadinta@gmail.com>
2017-05-19 15:17:16 +0300

    remove unused var

--- src/containers/map/layer-list.js
+++ src/containers/map/layer-list.js
@@ -1,34 +1,33 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
-import {connect} from 'react-redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import MapControl from '../../components/map-controls';
 import LayersList from '../../components/map-controls/layers-list';
 
-
 const layerListStyle = {
     position: 'absolute',
     top: '1rem',
-    left: '1rem'
+    left: '1rem',
 };
 
 class LayerList extends Component {
-    render () {
-        const {objectsDataFilter, onChangeItem} = this.props;
+    render() {
+        const { objectsDataFilter, onChangeItem } = this.props;
         return (
             <MapControl style={layerListStyle}>
-                <LayersList objectsDataFilter={objectsDataFilter} onChangeItem={onChangeItem}/>
+                <LayersList
+                    objectsDataFilter={objectsDataFilter}
+                    onChangeItem={onChangeItem}
+                />
             </MapControl>
-        )
+        );
     }
 }
 
-const mapProps = ({map}) => ({
-    map
+const mapProps = ({ map }) => ({
+    map,
 });
 
-const mapActions = {
-
-};
+const mapActions = {};
 
 export default connect(mapProps, mapActions)(LayerList);
