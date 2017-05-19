f70970e0453d9d4e4ecd85e38ca5be0e7da104a9
TPoryadin <poryadinta@gmail.com>
2017-05-19 15:01:19 +0300

    prettier test

--- src/index.js
+++ src/index.js
@@ -21,16 +21,16 @@ numeral.locale('ru');
 const store = configureStore();
 
 const render = Component => {
-  ReactDOM.render(
-    <AppContainer>
-      <Component store={store} />
-    </AppContainer>,
-    document.getElementById('root'),
-  );
+    ReactDOM.render(
+        <AppContainer>
+            <Component store={store} />
+        </AppContainer>,
+        document.getElementById('root'),
+    );
 };
 
 render(Root);
 
 if (module.hot) {
-  module.hot.accept('./Root', () => render(Root));
+    module.hot.accept('./Root', () => render(Root));
 }
