18b26196f63bd1d1f9313023bf9425a071b084c0
kreiz0 <thekreizo@gmail.com>
2017-06-07 15:28:14 +0300

    tasks empty

--- src/components/inspections-content/plan-next-month.js
+++ src/components/inspections-content/plan-next-month.js
@@ -12,33 +12,56 @@ const primaryButtonStyle = {
     border: 'none',
 };
 
+const planButtonStyle = {
+    width: 178,
+    margin: 0,
+    boxShadow: 'none',
+    border: 'none',
+};
+
 const labelPrimaryButtonStyle = {
     padding: 0,
     textTransform: 'none',
     fontWeight: 500,
 };
 
-const PlanMonth = ({ tasks }) => (
-    <div className="plan-next-month-wrapper">
-        <div className="plan-next-month-block">
-            {tasks &&
-                Object.keys(tasks).map(date => (
-                    <DateBlock
-                        date={date}
-                        tasks={tasks[date]}
-                        key={date + '-key'}
-                    />
-                ))}
+const PlanMonth = ({ tasks }) => {
+    return (
+        <div className="plan-next-month-wrapper">
+            {Object.keys(tasks).length === 0
+                ? <div className="tasks-empty">
+                      <div className="tasks-empty-title">
+                          Нет задач на согласовании
+                      </div>
+                      <RoundedButton
+                          style={planButtonStyle}
+                          labelStyle={labelPrimaryButtonStyle}
+                          label="Cформировать план"
+                          primary={true}
+                      />
+                  </div>
+                : <div>
+                      <div className="plan-next-month-block">
+                          {tasks &&
+                              Object.keys(tasks).map(date => (
+                                  <DateBlock
+                                      date={date}
+                                      tasks={tasks[date]}
+                                      key={date + '-key'}
+                                  />
+                              ))}
+                      </div>
+                      <div className="plan-next-month-footer">
+                          <RoundedButton
+                              style={primaryButtonStyle}
+                              labelStyle={labelPrimaryButtonStyle}
+                              label="Согласовать все задачи"
+                              primary={true}
+                          />
+                      </div>
+                  </div>}
         </div>
-        <div className="plan-next-month-footer">
-            <RoundedButton
-                style={primaryButtonStyle}
-                labelStyle={labelPrimaryButtonStyle}
-                label="Согласовать все задачи"
-                primary={true}
-            />
-        </div>
-    </div>
-);
+    );
+};
 
 export default withLoader(PlanMonth);
